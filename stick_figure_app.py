b1f8d2ef94b28e96931024b437b541bbc707115c
中村勇哉 <nakamurayuya@nakamuMacBook-Air.local>
2023-01-25 23:00:28 +0900

    new commit

--- stick_figure_app.py
+++ stick_figure_app.py
@@ -172,8 +172,8 @@ ctx = webrtc_streamer(
 )
 if ctx.video_processor:
     st.write("Parameters")
-    ctx.video_processor.RADIUS = int(st.slider("顔の大きさ", min_value=1.0, max_value=500.0, step=1, value=100.0))
-    ctx.video_processor.THICKNESS = int(st.slider("手足の太さ", min_value=1.0, max_value=100.0, step=1, value=50.0))
+    ctx.video_processor.RADIUS = int(st.slider("顔の大きさ", min_value=1.0, max_value=500.0, step=1.0, value=100.0))
+    ctx.video_processor.THICKNESS = int(st.slider("手足の太さ", min_value=1.0, max_value=100.0, step=1.0, value=50.0))
     ctx.video_processor.R = st.slider('赤', min_value=0, max_value=255, value=0)
     ctx.video_processor.G = st.slider('緑', min_value=0, max_value=255, value=255)
     ctx.video_processor.B = st.slider('青', min_value=0, max_value=255, value=0)
