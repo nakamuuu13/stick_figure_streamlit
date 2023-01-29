d082e6adb44901d14ea10aea6aa4fc958d045737
中村勇哉 <nakamurayuya@nakamuMacBook-Air.local>
2023-01-29 23:04:10 +0900

    initial value rev

--- stick_figure_app.py
+++ stick_figure_app.py
@@ -51,8 +51,8 @@ elif option == "棒人間":
     )
     if ctx.video_processor:
         st.write("Parameters")
-        ctx.video_processor.radius = int(st.slider("顔の大きさ", min_value=1.0, max_value=500.0, step=1.0, value=100.0))
-        ctx.video_processor.thickness = int(st.slider("手足の太さ", min_value=1.0, max_value=100.0, step=1.0, value=50.0))
+        ctx.video_processor.radius = int(st.slider("顔の大きさ", min_value=1.0, max_value=500.0, step=1.0, value=30.0))
+        ctx.video_processor.thickness = int(st.slider("手足の太さ", min_value=1.0, max_value=100.0, step=1.0, value=15.0))
         ctx.video_processor.R = st.slider('赤', min_value=0, max_value=255, value=0)
         ctx.video_processor.G = st.slider('緑', min_value=0, max_value=255, value=255)
         ctx.video_processor.B = st.slider('青', min_value=0, max_value=255, value=0)
